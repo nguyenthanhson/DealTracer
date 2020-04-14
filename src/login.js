ec992fd85c059621eb3c4755812d7e44c390355b
lynguyen0809 <lymisk11@gmail>
2020-04-14 15:38:51 +0700

    Ly Nguyen commit login function

--- src/login.js
+++ src/login.js
@@ -1,5 +1,12 @@
 const puppeteer = require("puppeteer");
-const { expect } = require("chai");
+const CREDS = require("../creds");
+const btnCancel = "#onesignal-popover-cancel-button";
+const lblLoginHP = "div:nth-child(3) > span > span";
+const btnLoginHP = "div:nth-child(3) > div > button:nth-child(1)";
+const txtEmail = "#email";
+const txtPassword = "#password";
+const btnLogin =
+  "form > div:nth-child(3) > button.UserModalstyle__RightButton-uq4a18-8.jpHvvv";
 
 const pageURL = "https://tiki.vn";
 (async () => {
@@ -9,42 +16,27 @@ const pageURL = "https://tiki.vn";
     args: ["--start-maximized", "--no-sandbox"],
   });
   const page = await browser.newPage();
-  try {
-    await page.goto(pageURL, { waitUntil: "networkidle2" });
-    const btnCancel = "#onesignal-popover-cancel-button";
-    await page.waitForSelector(btnCancel, {
-      visible: true,
-      timeout: 5000,
-    });
-    await page.click(btnCancel);
+  await page.goto(pageURL, { waitUntil: "networkidle2" });
 
-    const lblLoginHP = "div:nth-child(3) > span > span";
-    const btnLoginHP = "div:nth-child(3) > div > button:nth-child(1)";
-    await page.hover(lblLoginHP);
-    await page.waitFor(1000);
-    await page.waitForSelector(btnLoginHP);
-    await page.click(btnLoginHP);
+  //Cancel initial the popup
+  await page.waitForSelector(btnCancel, {
+    visible: true,
+    timeout: 5000,
+  });
+  await page.click(btnCancel);
 
-    const txtEmail = "#email";
-    const txtPassword = "#password";
-    const btnLogin =
-      "form > div:nth-child(3) > button.UserModalstyle__RightButton-uq4a18-8.jpHvvv";
-    await page.waitForSelector(txtEmail);
-    await page.type(txtEmail, "littlesnail0809@gmail.com");
-    await page.type(txtPassword, "dealTracer01");
-    await page.click(btnLogin);
-    await page.waitForNavigation();
-  } catch (e) {
-    console.log(e);
-  }
-
+  //Navigate to Login form
+  await page.hover(lblLoginHP);
   await page.waitFor(1000);
-  const lblWelcome = await page.$eval(
-    "div:nth-child(3) > span > span",
-    (el) => el.innerText
-  );
-  expect(lblWelcome).to.eql("Chào Ly dealTracer");
+  await page.waitForSelector(btnLoginHP);
+  await page.click(btnLoginHP);
 
-  await page.screenshot({ path: "example.png" });
+  //Submit login form
+  await page.waitForSelector(txtEmail);
+  await page.type(txtEmail, CREDS.user);
+  await page.type(txtPassword, CREDS.pass);
+  await page.click(btnLogin);
+  await page.waitForNavigation();
+
   await browser.close();
 })();
